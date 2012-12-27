5fe2874f3c82134babf20d1dddcf6622e3f9afc6
Vasily i. Redkin <vir@ctm.ru>
2012-12-27 10:07:38 +0400

    Fixed win32 libedixml: include libedi, libedistruct, expat and iconv.

--- libedixml/output.h
+++ libedixml/output.h
@@ -5,7 +5,7 @@ void output_init(char ** bufptr);
 int output_set_enc(const char * e);
 void output_shutdown();
 void output(const char * format, ...);
-
+char * escape_xml(const char * src);
 
 #endif /* OUTPUT_H_INCLUDED */
 
